82dd526c2cb72cfc920f094adebd82fb77a3abfc
JoJoPnz <act21078@gmail.com>
2022-12-12 15:58:26 +0700

    implement my_market page controller test

--- test/controllers/markets_controller_test.rb
+++ test/controllers/markets_controller_test.rb
@@ -1,9 +1,73 @@
 require "test_helper"
 
 class MarketsControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @market = markets(:one)
-  # end
+  setup do
+    sign_in_as users(:buyer1)
+  end
+
+  # my_market page
+  test "should show information" do
+    get my_market_url
+    assert_response :success
+  end
+
+  test "should not buy item if quantity less than or equal zero" do
+    @item = items(:electronic1_enable)
+    assert_no_difference 'Inventory.count' do
+      post purchase_item_url(qty: -1, item_id: @item.id)
+    end
+    assert_redirected_to my_market_url
+    assert_equal "quantity must be greater than 0", flash[:error]
+  end
+
+  test "should not buy item if item stock is less than quantity" do
+    @item = items(:electronic1_enable)
+    @market = Market.where(item_id: @item.id).first
+    @stock = @market.stock
+    @qty = @stock + 1
+    assert_no_difference 'Inventory.count' do
+      post purchase_item_url(qty: @qty, item_id: @item.id)
+    end
+    assert_redirected_to my_market_url
+    assert_equal "item stock is less than the quantity you want to buy", flash[:error]
+  end
+
+  test "should buy item successfully" do
+    @item = items(:electronic1_enable)
+    @market = Market.where(item_id: @item.id).first
+    @stock = @market.stock
+    @qty = @stock
+    assert_difference 'Inventory.count', 1 do
+      post purchase_item_url(qty: @qty, item_id: @item.id)
+    end
+    assert_redirected_to my_market_url
+    assert_equal 'purchase ' + @qty.to_s + ' ' + @item.name.to_s + ' successfully', flash[:success]
+  end
+
+  # my_inventory page
+
+
+
+  # Authentication test
+  test "should not be able to access my_market page if not login" do
+    get logout_url
+    get my_market_url
+    assert_redirected_to login_url
+    assert_equal 'you must be login first', flash[:notice]
+  end
+
+  # Authorization test
+  test "should not be able to access my_market page if login with seller account" do
+    get logout_url
+    sign_in_as users(:seller1)
+    get my_market_url
+    assert_redirected_to main_url
+    assert_equal 'you must be buyer or admin to access this page', flash[:notice]
+  end
+
+
+
+
 
   # test "should get index" do
   #   get markets_url
