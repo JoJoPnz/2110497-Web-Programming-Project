9d7c4b464c1faea649723b944b1b42751e2850e4
JoJoPnz <act21078@gmail.com>
2022-12-12 14:49:04 +0700

    implement my_market system test

---
+++ test/system/my_market_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class MyMarketTest < ApplicationSystemTestCase
+  setup do
+    @buyer = users(:buyer1)
+    visit login_url
+    fill_in "email", with: @buyer.email
+    fill_in "password", with: "password"
+    click_on "OK"
+  end
+
+#   test "should show information" do
+#     visit my_market_url
+#     assert_selector "#my_market_table"
+#   end
+
+#   test "should search by category" do
+#     visit my_market_url
+#     @item = items(:electronic1_enable)
+#     assert_selector "label", text: "Search By Category"
+#     fill_in "Search By Category", with: @item.category
+#     assert_selector "td", text: @item.category
+#   end
+
+end
