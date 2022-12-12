609d1d16364bb092063e9ed7e735d7577038bba2
JoJoPnz <act21078@gmail.com>
2022-12-12 14:46:11 +0700

    comment all auto generate scaffold test

--- test/system/inventories_test.rb
+++ test/system/inventories_test.rb
@@ -1,49 +1,49 @@
 require "application_system_test_case"
 
 class InventoriesTest < ApplicationSystemTestCase
-  setup do
-    @inventory = inventories(:one)
-  end
+  # setup do
+  #   @inventory = inventories(:one)
+  # end
 
-  test "visiting the index" do
-    visit inventories_url
-    assert_selector "h1", text: "Inventories"
-  end
+  # test "visiting the index" do
+  #   visit inventories_url
+  #   assert_selector "h1", text: "Inventories"
+  # end
 
-  test "should create inventory" do
-    visit inventories_url
-    click_on "New inventory"
+  # test "should create inventory" do
+  #   visit inventories_url
+  #   click_on "New inventory"
 
-    fill_in "Item", with: @inventory.item_id
-    fill_in "Price", with: @inventory.price
-    fill_in "Qty", with: @inventory.qty
-    fill_in "Timestamp", with: @inventory.timestamp
-    fill_in "User", with: @inventory.user_id
-    click_on "Create Inventory"
+  #   fill_in "Item", with: @inventory.item_id
+  #   fill_in "Price", with: @inventory.price
+  #   fill_in "Qty", with: @inventory.qty
+  #   fill_in "Timestamp", with: @inventory.timestamp
+  #   fill_in "User", with: @inventory.user_id
+  #   click_on "Create Inventory"
 
-    assert_text "Inventory was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Inventory was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "should update Inventory" do
-    visit inventory_url(@inventory)
-    click_on "Edit this inventory", match: :first
+  # test "should update Inventory" do
+  #   visit inventory_url(@inventory)
+  #   click_on "Edit this inventory", match: :first
 
-    fill_in "Item", with: @inventory.item_id
-    fill_in "Price", with: @inventory.price
-    fill_in "Qty", with: @inventory.qty
-    fill_in "Timestamp", with: @inventory.timestamp
-    fill_in "User", with: @inventory.user_id
-    click_on "Update Inventory"
+  #   fill_in "Item", with: @inventory.item_id
+  #   fill_in "Price", with: @inventory.price
+  #   fill_in "Qty", with: @inventory.qty
+  #   fill_in "Timestamp", with: @inventory.timestamp
+  #   fill_in "User", with: @inventory.user_id
+  #   click_on "Update Inventory"
 
-    assert_text "Inventory was successfully updated"
-    click_on "Back"
-  end
+  #   assert_text "Inventory was successfully updated"
+  #   click_on "Back"
+  # end
 
-  test "should destroy Inventory" do
-    visit inventory_url(@inventory)
-    click_on "Destroy this inventory", match: :first
+  # test "should destroy Inventory" do
+  #   visit inventory_url(@inventory)
+  #   click_on "Destroy this inventory", match: :first
 
-    assert_text "Inventory was successfully destroyed"
-  end
+  #   assert_text "Inventory was successfully destroyed"
+  # end
 end
--- test/system/markets_test.rb
+++ test/system/markets_test.rb
@@ -1,47 +1,47 @@
 require "application_system_test_case"
 
 class MarketsTest < ApplicationSystemTestCase
-  setup do
-    @market = markets(:one)
-  end
+  # setup do
+  #   @market = markets(:one)
+  # end
 
-  test "visiting the index" do
-    visit markets_url
-    assert_selector "h1", text: "Markets"
-  end
+  # test "visiting the index" do
+  #   visit markets_url
+  #   assert_selector "h1", text: "Markets"
+  # end
 
-  test "should create market" do
-    visit markets_url
-    click_on "New market"
+  # test "should create market" do
+  #   visit markets_url
+  #   click_on "New market"
 
-    fill_in "Item", with: @market.item_id
-    fill_in "Price", with: @market.price
-    fill_in "Stock", with: @market.stock
-    fill_in "User", with: @market.user_id
-    click_on "Create Market"
+  #   fill_in "Item", with: @market.item_id
+  #   fill_in "Price", with: @market.price
+  #   fill_in "Stock", with: @market.stock
+  #   fill_in "User", with: @market.user_id
+  #   click_on "Create Market"
 
-    assert_text "Market was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Market was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "should update Market" do
-    visit market_url(@market)
-    click_on "Edit this market", match: :first
+  # test "should update Market" do
+  #   visit market_url(@market)
+  #   click_on "Edit this market", match: :first
 
-    fill_in "Item", with: @market.item_id
-    fill_in "Price", with: @market.price
-    fill_in "Stock", with: @market.stock
-    fill_in "User", with: @market.user_id
-    click_on "Update Market"
+  #   fill_in "Item", with: @market.item_id
+  #   fill_in "Price", with: @market.price
+  #   fill_in "Stock", with: @market.stock
+  #   fill_in "User", with: @market.user_id
+  #   click_on "Update Market"
 
-    assert_text "Market was successfully updated"
-    click_on "Back"
-  end
+  #   assert_text "Market was successfully updated"
+  #   click_on "Back"
+  # end
 
-  test "should destroy Market" do
-    visit market_url(@market)
-    click_on "Destroy this market", match: :first
+  # test "should destroy Market" do
+  #   visit market_url(@market)
+  #   click_on "Destroy this market", match: :first
 
-    assert_text "Market was successfully destroyed"
-  end
+  #   assert_text "Market was successfully destroyed"
+  # end
 end
